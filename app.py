7a8ca4937e40e3dbe77982b504dd50670c858122
AHADGH001 <99137282+AHADGH001@users.noreply.github.com>
2024-07-15 16:58:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load pre-trained machine learning model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Extract features from form
+        age = float(request.form['age'])
+        sex = float(request.form['sex'])
+        cp = float(request.form['cp'])
+        trestbps = float(request.form['trestbps'])
+        chol = float(request.form['chol'])
+        fbs = float(request.form['fbs'])
+        restecg = float(request.form['restecg'])
+        thalach = float(request.form['thalach'])
+        exang = float(request.form['exang'])
+        oldpeak = float(request.form['oldpeak'])
+        slope = float(request.form['slope'])
+        ca = float(request.form['ca'])
+        thal = float(request.form['thal'])
+
+        # Make prediction
+        features = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        prediction = model.predict(features)
+
+        # Render prediction result
+        return render_template('result.html', prediction=prediction)
+
+if __name__ == '_main_':
+    app.run(debug=True)
